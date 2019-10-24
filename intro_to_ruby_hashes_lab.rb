d6483bbb77f8ded6acf957122772b6c629d76c51
Owen Cruse <ocruse7@gmail.com>
2019-10-24 19:05:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,9 +45,10 @@ def monopoly_with_fourth_tier
   my_hash = {
 	  :railroads => {
 	    :names => { 
-	      :reading_railroad => {}
-	      :pennsylvania_railroad => {}
-	      :
+	      :reading_railroad => {},
+	      :pennsylvania_railroad => {},
+	      :b_and_o_railroad => {},
+	      :shortline_railroad => {}
 	    }
 	    :pieces => 4, 
 	    :rent_in_dollars =>{
